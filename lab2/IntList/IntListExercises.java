04cff3d324346bff3c746084622208a6d886f3cc
Mengping Zhang <zmp040305@gmail.com>
2025-09-06 18:47:55 -0400

    Finished lab2

--- lab2/IntList/IntListExercises.java
+++ lab2/IntList/IntListExercises.java
@@ -10,7 +10,7 @@ public class IntListExercises {
      */
     public static void addConstant(IntList lst, int c) {
         IntList head = lst;
-        while (head.rest != null) {
+        while (head != null) {
             head.first += c;
             head = head.rest;
         }
@@ -33,7 +33,9 @@ public class IntListExercises {
         }
     }
 
-    /** Returns the max value in the IntList starting at L. */
+    /**
+     * Returns the max value in the IntList starting at L.
+     */
     public static int max(IntList L) {
         int max = L.first;
         IntList p = L.rest;
@@ -46,12 +48,13 @@ public class IntListExercises {
         return max;
     }
 
-    /** Returns true if the last digit of x is equal to
-     *  the first digit of x.
+    /**
+     * Returns true if the last digit of x is equal to
+     * the first digit of x.
      */
     public static boolean firstDigitEqualsLastDigit(int x) {
         int lastDigit = x % 10;
-        while (x > 10) {
+        while (x >= 10) {
             x = x / 10;
         }
         int firstDigit = x % 10;
@@ -66,17 +69,23 @@ public class IntListExercises {
      * @return True if there was an update to the list
      */
     public static boolean squarePrimes(IntList lst) {
-        // Base Case: we have reached the end of the list
-        if (lst == null) {
-            return false;
+        /**
+         * Part C: (Buggy) mutative method that squares each prime
+         * element of the IntList.
+         *
+         * @param lst IntList from Lecture
+         * @return True if there was an update to the list
+         */
+        boolean hasPrime = false;
+        IntList current = lst;
+
+        while (current != null) {
+            if (Primes.isPrime(current.first)) {
+                current.first *= current.first;
+                hasPrime = true;
+            }
+            current = current.rest;
         }
-
-        boolean currElemIsPrime = Primes.isPrime(lst.first);
-
-        if (currElemIsPrime) {
-            lst.first *= lst.first;
-        }
-
-        return currElemIsPrime || squarePrimes(lst.rest);
+        return hasPrime;
     }
 }
--- lab2/IntList/SquarePrimesTest.java
+++ lab2/IntList/SquarePrimesTest.java
@@ -17,4 +17,36 @@ public class SquarePrimesTest {
         assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst.toString());
         assertTrue(changed);
     }
+
+    @Test
+    public void testSquarePrimesDouble() {
+        IntList lst = IntList.of(14, 15, 17, 17, 18);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("14 -> 15 -> 289 -> 289 -> 18", lst.toString());
+        assertTrue(changed);
+    }
+
+    @Test
+    public void testSquarePrimesTriple() {
+        IntList lst = IntList.of(14, 7, 17, 17, 18);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("14 -> 49 -> 289 -> 289 -> 18", lst.toString());
+        assertTrue(changed);
+    }
+
+    @Test
+    public void testSquarePrimesLastOne() {
+        IntList lst = IntList.of(14, 7, 17, 17, 5);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("14 -> 49 -> 289 -> 289 -> 25", lst.toString());
+        assertTrue(changed);
+    }
+
+    @Test
+    public void testSquarePrimesNoPrime() {
+        IntList lst = IntList.of(4,4,4);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("4 -> 4 -> 4", lst.toString());
+        assertTrue(!changed);
+    }
 }
